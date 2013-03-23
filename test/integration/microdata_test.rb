33356c8b2ce76971315403c65eebfeaa260972a7
Jason Ronallo <jronallo@gmail.com>
2013-03-22 22:22:49 -0400

    first properties for the person

--- test/integration/microdata_test.rb
+++ test/integration/microdata_test.rb
@@ -10,5 +10,12 @@ class MicrodataTest < ActionDispatch::IntegrationTest
   test "the top level item is a schema.org/Person" do
     assert_equal ['http://schema.org/Person'], @items.first.type
   end
-  
+
+  test "the Person should have a name, url, and job title" do
+    person = @items.first
+    assert_equal ['Jason Ronallo'], person.properties['name']
+    assert_equal ['http://twitter.com/ronallo'], person.properties['url']
+    assert_equal ['Associate Head of Digital Library Initiatives'], person.properties['jobTitle']
+  end
+
 end
